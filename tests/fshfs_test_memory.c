131c092beba858798c91a498b23a0d775de38de9
Joachim Metz <joachim.metz@gmail.com>
2016-10-05 07:14:42 +0200

    Worked on tests and Windows test scripts.

--- tests/fshfs_test_memory.c
+++ tests/fshfs_test_memory.c
@@ -55,8 +55,8 @@ void *malloc(
 	if( fshfs_test_real_malloc == NULL )
 	{
 		fshfs_test_real_malloc = dlsym(
-		                           RTLD_NEXT,
-		                           "malloc" );
+		                          RTLD_NEXT,
+		                          "malloc" );
 	}
 	if( fshfs_test_malloc_attempts_before_fail == 0 )
 	{
@@ -86,8 +86,8 @@ void *memcpy(
 	if( fshfs_test_real_memcpy == NULL )
 	{
 		fshfs_test_real_memcpy = dlsym(
-		                           RTLD_NEXT,
-		                           "memcpy" );
+		                          RTLD_NEXT,
+		                          "memcpy" );
 	}
 	if( fshfs_test_memcpy_attempts_before_fail == 0 )
 	{
@@ -119,8 +119,8 @@ void *memset(
 	if( fshfs_test_real_memset == NULL )
 	{
 		fshfs_test_real_memset = dlsym(
-		                           RTLD_NEXT,
-		                           "memset" );
+		                          RTLD_NEXT,
+		                          "memset" );
 	}
 	if( fshfs_test_memset_attempts_before_fail == 0 )
 	{
@@ -151,8 +151,8 @@ void *realloc(
 	if( fshfs_test_real_realloc == NULL )
 	{
 		fshfs_test_real_realloc = dlsym(
-		                            RTLD_NEXT,
-		                            "realloc" );
+		                           RTLD_NEXT,
+		                           "realloc" );
 	}
 	if( fshfs_test_realloc_attempts_before_fail == 0 )
 	{
