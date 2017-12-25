ad0408b566d3a0fa6310bd3698f3915f5138a56a
Joachim Metz <joachim.metz@gmail.com>
2017-12-25 18:42:44 +0100

    Worked on tests

---
+++ tests/fshfs_test_btree_header.c
@@ -0,0 +1,476 @@
+/*
+ * Library btree_header type test program
+ *
+ * Copyright (C) 2009-2017, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <file_stream.h>
+#include <types.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( WINAPI )
+#include <stdlib.h>
+#endif
+
+#include "fshfs_test_libcerror.h"
+#include "fshfs_test_libfshfs.h"
+#include "fshfs_test_macros.h"
+#include "fshfs_test_memory.h"
+#include "fshfs_test_unused.h"
+
+#include "../libfshfs/libfshfs_btree_header.h"
+
+uint8_t fshfs_test_btree_header_data1[ 106 ] = {
+	0x00, 0x02, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00,
+	0x00, 0x01, 0x10, 0x00, 0x02, 0x04, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x01, 0xf7, 0x00, 0x00,
+	0x00, 0x20, 0x00, 0x00, 0x00, 0xcf, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+#if defined( __GNUC__ ) && !defined( LIBFSHFS_DLL_IMPORT )
+
+/* Tests the libfshfs_btree_header_initialize function
+ * Returns 1 if successful or 0 if not
+ */
+int fshfs_test_btree_header_initialize(
+     void )
+{
+	libcerror_error_t *error              = NULL;
+	libfshfs_btree_header_t *btree_header = NULL;
+	int result                            = 0;
+
+#if defined( HAVE_FSHFS_TEST_MEMORY )
+	int number_of_malloc_fail_tests       = 1;
+	int number_of_memset_fail_tests       = 1;
+	int test_number                       = 0;
+#endif
+
+	/* Test regular cases
+	 */
+	result = libfshfs_btree_header_initialize(
+	          &btree_header,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "btree_header",
+	 btree_header );
+
+	FSHFS_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	result = libfshfs_btree_header_free(
+	          &btree_header,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	FSHFS_TEST_ASSERT_IS_NULL(
+	 "btree_header",
+	 btree_header );
+
+	FSHFS_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = libfshfs_btree_header_initialize(
+	          NULL,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	btree_header = (libfshfs_btree_header_t *) 0x12345678UL;
+
+	result = libfshfs_btree_header_initialize(
+	          &btree_header,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	btree_header = NULL;
+
+#if defined( HAVE_FSHFS_TEST_MEMORY )
+
+	for( test_number = 0;
+	     test_number < number_of_malloc_fail_tests;
+	     test_number++ )
+	{
+		/* Test libfshfs_btree_header_initialize with malloc failing
+		 */
+		fshfs_test_malloc_attempts_before_fail = test_number;
+
+		result = libfshfs_btree_header_initialize(
+		          &btree_header,
+		          &error );
+
+		if( fshfs_test_malloc_attempts_before_fail != -1 )
+		{
+			fshfs_test_malloc_attempts_before_fail = -1;
+
+			if( btree_header != NULL )
+			{
+				libfshfs_btree_header_free(
+				 &btree_header,
+				 NULL );
+			}
+		}
+		else
+		{
+			FSHFS_TEST_ASSERT_EQUAL_INT(
+			 "result",
+			 result,
+			 -1 );
+
+			FSHFS_TEST_ASSERT_IS_NULL(
+			 "btree_header",
+			 btree_header );
+
+			FSHFS_TEST_ASSERT_IS_NOT_NULL(
+			 "error",
+			 error );
+
+			libcerror_error_free(
+			 &error );
+		}
+	}
+	for( test_number = 0;
+	     test_number < number_of_memset_fail_tests;
+	     test_number++ )
+	{
+		/* Test libfshfs_btree_header_initialize with memset failing
+		 */
+		fshfs_test_memset_attempts_before_fail = test_number;
+
+		result = libfshfs_btree_header_initialize(
+		          &btree_header,
+		          &error );
+
+		if( fshfs_test_memset_attempts_before_fail != -1 )
+		{
+			fshfs_test_memset_attempts_before_fail = -1;
+
+			if( btree_header != NULL )
+			{
+				libfshfs_btree_header_free(
+				 &btree_header,
+				 NULL );
+			}
+		}
+		else
+		{
+			FSHFS_TEST_ASSERT_EQUAL_INT(
+			 "result",
+			 result,
+			 -1 );
+
+			FSHFS_TEST_ASSERT_IS_NULL(
+			 "btree_header",
+			 btree_header );
+
+			FSHFS_TEST_ASSERT_IS_NOT_NULL(
+			 "error",
+			 error );
+
+			libcerror_error_free(
+			 &error );
+		}
+	}
+#endif /* defined( HAVE_FSHFS_TEST_MEMORY ) */
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	if( btree_header != NULL )
+	{
+		libfshfs_btree_header_free(
+		 &btree_header,
+		 NULL );
+	}
+	return( 0 );
+}
+
+/* Tests the libfshfs_btree_header_free function
+ * Returns 1 if successful or 0 if not
+ */
+int fshfs_test_btree_header_free(
+     void )
+{
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test error cases
+	 */
+	result = libfshfs_btree_header_free(
+	          NULL,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
+}
+
+/* Tests the libfshfs_btree_header_read_data function
+ * Returns 1 if successful or 0 if not
+ */
+int fshfs_test_btree_header_read_data(
+     void )
+{
+	libcerror_error_t *error              = NULL;
+	libfshfs_btree_header_t *btree_header = NULL;
+	int result                            = 0;
+
+	/* Initialize test
+	 */
+	result = libfshfs_btree_header_initialize(
+	          &btree_header,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "btree_header",
+	 btree_header );
+
+	FSHFS_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test regular cases
+	 */
+	result = libfshfs_btree_header_read_data(
+	          btree_header,
+	          fshfs_test_btree_header_data1,
+	          106,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	FSHFS_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = libfshfs_btree_header_read_data(
+	          NULL,
+	          fshfs_test_btree_header_data1,
+	          106,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	result = libfshfs_btree_header_read_data(
+	          btree_header,
+	          NULL,
+	          106,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	result = libfshfs_btree_header_read_data(
+	          btree_header,
+	          fshfs_test_btree_header_data1,
+	          (size_t) SSIZE_MAX + 1,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	result = libfshfs_btree_header_read_data(
+	          btree_header,
+	          fshfs_test_btree_header_data1,
+	          0,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	FSHFS_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	/* Clean up
+	 */
+	result = libfshfs_btree_header_free(
+	          &btree_header,
+	          &error );
+
+	FSHFS_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	FSHFS_TEST_ASSERT_IS_NULL(
+	 "btree_header",
+	 btree_header );
+
+	FSHFS_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	if( btree_header != NULL )
+	{
+		libfshfs_btree_header_free(
+		 &btree_header,
+		 NULL );
+	}
+	return( 0 );
+}
+
+#endif /* defined( __GNUC__ ) && !defined( LIBFSHFS_DLL_IMPORT ) */
+
+/* The main program
+ */
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+int wmain(
+     int argc FSHFS_TEST_ATTRIBUTE_UNUSED,
+     wchar_t * const argv[] FSHFS_TEST_ATTRIBUTE_UNUSED )
+#else
+int main(
+     int argc FSHFS_TEST_ATTRIBUTE_UNUSED,
+     char * const argv[] FSHFS_TEST_ATTRIBUTE_UNUSED )
+#endif
+{
+	FSHFS_TEST_UNREFERENCED_PARAMETER( argc )
+	FSHFS_TEST_UNREFERENCED_PARAMETER( argv )
+
+#if defined( __GNUC__ ) && !defined( LIBFSHFS_DLL_IMPORT )
+
+	FSHFS_TEST_RUN(
+	 "libfshfs_btree_header_initialize",
+	 fshfs_test_btree_header_initialize );
+
+	FSHFS_TEST_RUN(
+	 "libfshfs_btree_header_free",
+	 fshfs_test_btree_header_free );
+
+	FSHFS_TEST_RUN(
+	 "libfshfs_btree_header_read_data",
+	 fshfs_test_btree_header_read_data );
+
+#endif /* defined( __GNUC__ ) && !defined( LIBFSHFS_DLL_IMPORT ) */
+
+	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
+}
+
--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -6,7 +6,7 @@ $ExitSuccess = 0
 $ExitFailure = 1
 $ExitIgnore = 77
 
-$LibraryTests = "allocation_block btree_file btree_node btree_node_descriptor directory_record error file_record fork_descriptor io_handle notify thread_record volume_header"
+$LibraryTests = "allocation_block btree_file btree_header btree_node btree_node_descriptor directory_record error file_record fork_descriptor io_handle notify thread_record volume_header"
 $LibraryTestsWithInput = "support volume"
 
 Function GetTestToolDirectory
