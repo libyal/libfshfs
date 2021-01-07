e80078f5ad0e60ac36796dba490246db8806a52d
Joachim Metz <joachim.metz@gmail.com>
2021-01-07 08:14:17 +0100

    Applied updates

--- libfshfs/fshfs_fork.h
+++ libfshfs/fshfs_fork.h
@@ -1,7 +1,7 @@
 /*
  * The HFS fork definitions
  *
- * Copyright (C) 2009-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
