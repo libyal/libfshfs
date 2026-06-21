68e001b07d8b152b3f0e3df5398f29d8ea4dfee2
Joachim Metz <joachim.metz@gmail.com>
2026-06-21 16:17:34 +0200

    Worked on tests and changes for CI tests

--- include/libfshfs/error.h
+++ include/libfshfs/error.h
@@ -82,7 +82,7 @@ enum LIBFSHFS_ARGUMENT_ERROR
 	 */
 	LIBFSHFS_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBFSHFS_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBFSHFS_OUTPUT_ERROR
 {
 	LIBFSHFS_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBFSHFS_OUTPUT_ERROR_INSUFFICIENT_SPACE	= 1
 };
