a13b37df313d4cb766184b672c5038bf9d01e17f
Joachim Metz <joachim.metz@gmail.com>
2026-06-26 09:10:51 +0200

    Worked on options and manpages

--- fshfstools/fshfsinfo.c
+++ fshfstools/fshfsinfo.c
@@ -21,11 +21,8 @@
 
 #include <common.h>
 #include <file_stream.h>
-#include <memory.h>
 #include <system_string.h>
 #include <types.h>
-
-#include <stdio.h>
 
 #if defined( HAVE_FCNTL_H ) || defined( WINAPI )
 #include <fcntl.h>
@@ -64,34 +61,6 @@ enum FSHFSINFO_MODES
 
 info_handle_t *fshfsinfo_info_handle = NULL;
 int fshfsinfo_abort                  = 0;
-
-/* Prints usage information
- */
-void usage_fprint(
-      FILE *stream )
-{
-	if( stream == NULL )
-	{
-		return;
-	}
-	fprintf( stream, "Use fshfsinfo to determine information about a Hierarchical\n"
-	                 " File System (HFS) volume.\n\n" );
-
-	fprintf( stream, "Usage: fshfsinfo [ -B bodyfile ] [ -E identifier ] [ -F file_entry ]\n"
-	                 "                 [ -o offset ] [ -dhHvV ] source\n\n" );
-
-	fprintf( stream, "\tsource: the source file or device\n\n" );
-
-	fprintf( stream, "\t-B:     output file system information as a bodyfile\n" );
-	fprintf( stream, "\t-d:     calculate a MD5 hash of a file entry to include in the bodyfile\n" );
-	fprintf( stream, "\t-E:     show information about a specific file system entry or \"all\"\n" );
-	fprintf( stream, "\t-F:     show information about a specific file entry path.\n" );
-	fprintf( stream, "\t-h:     shows this help\n" );
-	fprintf( stream, "\t-H:     shows the file system hierarchy\n" );
-	fprintf( stream, "\t-o:     specify the volume offset\n" );
-	fprintf( stream, "\t-v:     verbose output to stderr\n" );
-	fprintf( stream, "\t-V:     print version\n" );
-}
 
 /* Signal handler for fshfsinfo
  */
@@ -145,6 +114,23 @@ int wmain( int argc, wchar_t * const argv[] )
 int main( int argc, char * const argv[] )
 #endif
 {
+	const char *description = \
+		"Use fshfsinfo to determine information about a Hierarchical File System (HFS) volume.";
+
+	fshfstools_option_t options[ ] = {
+		{ 'B', "bodyfile", "output file system information as a bodyfile" },
+		{ 'd', NULL, "calculate a MD5 hash of a file entry to include in the bodyfile" },
+		{ 'E', "identifier", "show information about a specific file entry identifier or \"all\"" },
+		{ 'F', "path", "show information about a specific file entry path" },
+		{ 'h', NULL, "shows this help" },
+		{ 'H', NULL, "shows the file system hierarchy" },
+		{ 'o', "offset", "specify the volume offset in bytes" },
+		{ 'v', NULL, "verbose output to stderr" },
+		{ 'V', NULL, "print version" },
+		{ 0, "source", "the source volume" },
+	};
+	system_character_t options_string[ 32 ];
+
 	libfshfs_error_t *error                          = NULL;
 	system_character_t *option_bodyfile              = NULL;
 	system_character_t *option_file_entry_identifier = NULL;
@@ -157,6 +143,7 @@ int main( int argc, char * const argv[] )
 	uint64_t file_entry_identifier                   = 0;
 	uint8_t calculate_md5                            = 0;
 	int option_mode                                  = FSHFSINFO_MODE_VOLUME;
+	int number_of_options                            = (int) ( sizeof( options ) / sizeof( fshfstools_option_t ) );
 	int verbose                                      = 0;
 
 #if defined( __MINGW32__ ) && defined( HAVE_MINGW_BINMODE )
@@ -194,10 +181,22 @@ int main( int argc, char * const argv[] )
 	 stdout,
 	 program );
 
+	if( fshfstools_getopt_get_options_string(
+	     options,
+	     number_of_options,
+	     options_string,
+	     32 ) != 1 )
+	{
+		fprintf(
+		 stderr,
+		 "Unable to determine options string.\n" );
+
+		goto on_error;
+	}
 	while( ( option = fshfstools_getopt(
 	                   argc,
 	                   argv,
-	                   _SYSTEM_STRING( "B:dE:F:hHo:vV" ) ) ) != (system_integer_t) -1 )
+	                   options_string ) ) != (system_integer_t) -1 )
 	{
 		switch( option )
 		{
@@ -208,8 +207,12 @@ int main( int argc, char * const argv[] )
 				 "Invalid argument: %" PRIs_SYSTEM "\n",
 				 argv[ optind - 1 ] );
 
-				usage_fprint(
-				 stdout );
+				fshfstools_getopt_usage_fprint(
+				 stdout,
+				 program,
+				 description,
+				 options,
+				 number_of_options );
 
 				return( EXIT_FAILURE );
 
@@ -236,8 +239,12 @@ int main( int argc, char * const argv[] )
 				break;
 
 			case (system_integer_t) 'h':
-				usage_fprint(
-				 stdout );
+				fshfstools_getopt_usage_fprint(
+				 stdout,
+				 program,
+				 description,
+				 options,
+				 number_of_options );
 
 				return( EXIT_SUCCESS );
 
@@ -267,10 +274,14 @@ int main( int argc, char * const argv[] )
 	{
 		fprintf(
 		 stderr,
-		 "Missing source file or device.\n" );
+		 "Missing source volume.\n" );
 
-		usage_fprint(
-		 stdout );
+		fshfstools_getopt_usage_fprint(
+		 stdout,
+		 program,
+		 description,
+		 options,
+		 number_of_options );
 
 		return( EXIT_FAILURE );
 	}
